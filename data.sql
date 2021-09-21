0a0bf0aed3333b0d3ec8f4a0114936e4f3ecf437
Ghiftee <gigibetine@gmail.com>
2021-09-21 09:41:39 +0100

    Change weight value

--- data.sql
+++ data.sql
@@ -15,7 +15,7 @@ VALUES('Devimon', '2017-05-12', 5, true, 11);
 /* Query and update table */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES('Charmander', '2020-02-08', 0, false, 11.00);
+VALUES('Charmander', '2020-02-08', 0, false, 11);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES('Plantmon', '2022-11-15', 2, true, -5.7);
@@ -30,4 +30,4 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES('Boarmon', '2005-06-07', 7, true, 20.04);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES('Blossom', '1998-10-13', 3, true, 17.00);
+VALUES('Blossom', '1998-10-13', 3, true, 17);
