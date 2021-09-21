3aa12359d522a36d4be627612d7749ac3bdb1360
Ghiftee <gigibetine@gmail.com>
2021-09-21 08:41:26 +0100

    Add transactions

--- queries.sql
+++ queries.sql
@@ -15,3 +15,26 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* Query and update table */
+
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT DELETE_DOB;
+UPDATE animals SET weight_kg = (weight_kg * -1);
+ROLLBACK TO DELETE_DOB;
+UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0; 
+COMMIT TRANSACTION;
