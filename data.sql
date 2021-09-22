ec9a6a8532f84ae68a6fd486d5c36eaf1922b003
Ghiftee <gigibetine@gmail.com>
2021-09-22 15:36:30 +0100

    Modify animals to include species_id

--- data.sql
+++ data.sql
@@ -45,3 +45,9 @@ INSERT INTO species (name) VALUES('Pokemon'), ('Digimon');
 
 UPDATE animals SET species_id = 1 WHERE name LIKE '%mon';
 UPDATE animals SET species_id = 2 WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = 1 WHERE name LIKE 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
